e8208ccd2dfed6854d1497b01067c9cba7c5c946
Joel Hernandez <al02785443@tecmilenio.mx>
2021-02-17 19:22:33 -0700

    actividad 2 iOS

---
+++ Actividad3_JHC.playground/Contents.swift
@@ -0,0 +1,28 @@
+import UIKit
+//punto 1
+let entero:Int = 5
+var entero2 = 7
+let decimal:Float = 5.3
+var decimal2 = 4.32
+let cadenaEx:String = "Explicita"
+let cadenaIn = "Implicita"
+
+//punto 2
+var string = "string"
+var num = 1
+
+//punto 3
+var numeros:Array<Int> = Array<Int>()
+for n in 1...10{
+    numeros.append(n)
+}
+print(numeros)
+var diasSemana:Dictionary<Int, String> = Dictionary<Int, String>()
+diasSemana[1] = "Lunes"
+diasSemana[2] = "Martes"
+diasSemana[3] = "Miercoles"
+diasSemana[4] = "Jueves"
+diasSemana[5] = "Viernes"
+diasSemana[6] = "Sabado"
+diasSemana[7] = "Domingo"
+print(diasSemana)
