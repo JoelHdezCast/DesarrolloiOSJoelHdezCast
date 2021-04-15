924046de2a609218e399b9b2b7b40c8c49abb93d
Joel Hernandez <al02785443@tecmilenio.mx>
2021-04-14 22:18:37 -0600

    commit avance 2 evidencia

---
+++ evidenciaiOS_JHC/evidenciaiOS_JHC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  evidenciaiOS_JHC
+//
+//  Created by Francisco Hernandez Ayala on 14/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
